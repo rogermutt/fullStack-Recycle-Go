9743df25d06895cbea7582e30cc5871ab3f78909
rogermutt <r1ger_pg@hotmail.com>
2018-08-31 16:04:01 +1000

    Amended React Router

--- src/app/Components/Nav.js
+++ src/app/Components/Nav.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
+import { HashRouter, Link } from 'react-router-dom';
 
-class Footer extends Component {
-    render () {
-        return (
-              <nav>
-              <div className="nav-wrapper">
-                <a href="#" className="brand-logo">Logo</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li><a href="sass.html">Sass</a></li>
-                  <li><a href="badges.html">Components</a></li>
-                  <li><a href="collapsible.html">JavaScript</a></li>
-                </ul>
-              </div>
-            </nav>            
-            )
-        }
-}
-
-export default Footer;
+export default class Footer extends Component {
+  render () {
+      return (
+            <nav>
+            <div className="nav-wrapper">
+              <a href="#" className="brand-logo">Logo</a>
+              <HashRouter>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/main">Main</Link></li>
+                <li><Link to="/reports">Reports</Link></li>
+                <li><Link to="/select">Select</Link></li>
+              </ul>
+              </HashRouter>
+            </div>
+          </nav>            
+          )
+      }
+};
--- src/app/Components/_ReactRouter.js
+++ src/app/Components/_ReactRouter.js
@@ -4,12 +4,11 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Test from "./Test";
 import Registration from "./Registration";
 
-
 export default () => (
     <HashRouter>
         <Switch>
-            <Route exact path="/main" render={ (props) => <p>Landing</p> } /> 
             <Route exact path="/register" component={Registration} />
+            <Route exact path="/main" render={  (props) => <p>Main</p> } /> 
             <Route path="/select" component={Test} />
             <Route path="/reports" render={  (props) => <p>Select Items</p> } /> 
         </Switch>     
