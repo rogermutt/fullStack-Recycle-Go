f6352f3135c4c3080f8dfba5a30a224fe3395f92
rogermutt <r1ger_pg@hotmail.com>
2018-09-04 16:38:15 +1000

    Added Basic Grid

--- src/app/Components/_ReactRouter.js
+++ src/app/Components/_ReactRouter.js
@@ -3,12 +3,13 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Test from "./Test";
 import Registration from "./Registration";
+import Grid from "./Grid";
 
 export default () => (
     <HashRouter>
         <Switch>
             <Route exact path="/register" component={Registration} />
-            <Route exact path="/main" render={  (props) => <p>Main</p> } /> 
+            <Route exact path="/main" component={Grid} />
             <Route path="/select" component={Test} />
             <Route path="/reports" render={  (props) => <p>Select Items</p> } /> 
         </Switch>     
